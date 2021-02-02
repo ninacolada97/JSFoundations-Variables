e8c0875e3763353caae8ee8ba8f56975d35d0284
Mac <mac@Macs-MacBook-Pro.local>
2021-02-02 15:07:38 +0300

    task 3 completed

---
+++ story.js
@@ -0,0 +1,9 @@
+let name = "Haneen";
+let age = 24;
+let hobby = "reading";
+
+console.log(
+  `Mr./Mrs. ${name}, in 33 years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby}`
+);
